6ed1e81d8fccdee71be67c12fa8327b0bbdc6723
gpfeff <greg.a.pfeffer@gmail.com>
2014-02-28 00:45:24 -0600

    Deletes tictactoe.js and replaces with script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+<script>
+
+var sqr1
+var sqr2
+var sqr3
+var sqr4
+var sqr5
+var sqr6
+var sqr7
+var sqr8
+var sqr9
+//Creates variables for each one of the squares on the board// 
+
+
+var sqr1T = 0
+var sqr2T = 0
+var sqr3T = 0
+var sqr4T = 0
+var sqr5T = 0
+var sqr6T = 0
+var sqr7T = 0
+var sqr8T = 0
+var sqr9T = 0
+//Creates variables to keep track of weither a move has already taken a square or not, default = 0, taken = 1//
+
+
+
+var moveCount = 0
+//Creates variable to act as incremental counter to keep track of number of moves//
+
+
+</script>
+
